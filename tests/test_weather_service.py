b11b2829860c0daf195edf0486dc3966af1c6d17
Arun Rai <ciw8981@bb-usmia-v019-l-s-nafd-dscn01.krft.net>
2025-01-28 12:09:48 +0000

    refactor: remove unittest dependency and start using pytest

--- tests/test_weather_service.py
+++ tests/test_weather_service.py
@@ -1,34 +1,34 @@
-from unittest.mock import Mock, patch
-
 import pytest  # type: ignore
 from requests.exceptions import RequestException
 
 from src.weather_service import WeatherServiceError, get_weather
 
 
-@patch("src.weather_service.requests.get")
-def test_get_weather_success(mock_get):
+def test_get_weather_success(mocker):
     """
     Test that get_weather returns the correct temperature when the API call succeeds.
     """
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {"temperature": 22}
-    mock_get.return_value = mock_response
+    mocker.patch(
+        "requests.get",
+        return_value=mocker.MagicMock(
+            status_code=200, json=lambda: {"temperature": 22}
+        ),
+    )
 
     result = get_weather("New York", api_key="test_api_key")
     assert result == 22
 
 
-@patch("src.weather_service.requests.get")
-def test_get_weather_none_temperature(mock_get):
+def test_get_weather_none_temperature(mocker):
     """
     Test that get_weather raises WeatherServiceError if the temperature is None.
     """
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {"temperature": None}
-    mock_get.return_value = mock_response
+    mocker.patch(
+        "requests.get",
+        return_value=mocker.MagicMock(
+            status_code=200, json=lambda: {"temperature": None}
+        ),
+    )
 
     with pytest.raises(
         WeatherServiceError, match="Temperature data is missing in the response"
@@ -36,14 +36,17 @@ def test_get_weather_none_temperature(mock_get):
         get_weather("New York", api_key="test_api_key")
 
 
-@patch("src.weather_service.requests.get")
-def test_get_weather_failure(mock_get):
+def test_get_weather_failure(mocker):
     """
     Test that get_weather raises WeatherServiceError when the API call fails.
     """
-    mock_response = Mock()
-    mock_response.status_code = 404
-    mock_get.return_value = mock_response
+
+    mocker.patch(
+        "requests.get",
+        return_value=mocker.MagicMock(
+            status_code=404, json=lambda: {"temperature": None}
+        ),
+    )
 
     with pytest.raises(
         WeatherServiceError, match="Failed to fetch weather data for NonExistentCity"
@@ -51,41 +54,39 @@ def test_get_weather_failure(mock_get):
         get_weather("NonExistentCity", api_key="test_api_key")
 
 
-@patch("src.weather_service.requests.get")
-def test_get_weather_missing_temperature(mock_get):
+def test_get_weather_missing_temperature(mocker):
     """
     Test that get_weather raises WeatherServiceError if temperature is missing in the response.
     """
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {}
-    mock_get.return_value = mock_response
 
+    mocker.patch(
+        "requests.get",
+        return_value=mocker.MagicMock(status_code=200, json=lambda: {}),
+    )
     with pytest.raises(
         WeatherServiceError, match="Temperature data is missing in the response"
     ):
         get_weather("New York", api_key="test_api_key")
 
 
-@patch("src.weather_service.requests.get")
-def test_get_weather_json_parsing_error(mock_get):
+def test_get_weather_json_parsing_error(mocker):
     """
     Test that get_weather raises WeatherServiceError if JSON parsing fails.
     """
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.side_effect = ValueError
-    mock_get.return_value = mock_response
-
+    mocker.patch(
+        "requests.get",
+        return_value=mocker.MagicMock(status_code=200, json=lambda: "Invalid Json"),
+    )
+    
     with pytest.raises(WeatherServiceError, match="Failed to parse weather data"):
         get_weather("New York", api_key="test_api_key")
 
 
-@patch("src.weather_service.requests.get")
-def test_get_weather_request_exception(mock_get):
+def test_get_weather_request_exception(mocker):
     """
     Test that get_weather raises WeatherServiceError if a request exception occurs.
     """
+    mock_get = mocker.patch("requests.get")
     mock_get.side_effect = RequestException("Network error")
 
     with pytest.raises(
